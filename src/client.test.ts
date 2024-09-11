fb82c95d3885057eafdffab835c54061bd42f915
Florian Rüberg <florian.rueberg@gmail.com>
2024-09-11 19:00:46 +0200

    Test client

--- src/client.test.ts
+++ src/client.test.ts
@@ -1,39 +1,97 @@
 import { describe, it, expect, vi } from "vitest";
 import { SendenClient } from "./index";
 
-global.location = { origin: "http://localhost" } as any;
+async function assertFetch(root: string, call: (r: any) => Promise<any>, method: string, url: string, body: any = null) {
+  // Setup
 
-function mockFetch(response: any) {
-  const result = {
-    args: [] as any[],
-    fetch: vi.fn(),
-  };
+  let args = [] as any[];
+  let response = { unimportent: "response" };
+  let fetch = vi.fn();
 
-  result.fetch.mockImplementation(function () {
-    result.args = Array.from(arguments);
+  fetch.mockImplementation(function () {
+    args = Array.from(arguments);
     return { text: async () => JSON.stringify(response) };
   });
 
-  return result;
+  // Action
+
+  const client = new SendenClient();
+  client.fetch = fetch;
+  await call(client.build<any>(root));
+
+  // Assert
+  expect(fetch).toBeCalledTimes(1);
+  expect(args[0].method).toEqual(method);
+  expect(args[0].url).toEqual(url);
+  expect(await new Response(args[0].body || "null").json()).toEqual(body);
+  expect(Array.from(args[0].headers.entries())).toEqual([["content-type", "application/json"]]);
 }
 
-describe("Utility | Main", () => {
-  it("Calls fetch", async () => {
+describe("Test Client Fetch Requests", () => {
+  it("example?test=&abc=123", async () => {
     // Setup
-    const mock = mockFetch({});
+    global.location = { origin: "http://localhost" } as any;
+    const root = "/cdn/api";
+    const params = { $test: "", $abc: 123 };
+    const call = (r: any) => r.example.$get(params);
+    // Assert
+    const expected = "http://localhost/cdn/api/example?test=&abc=123";
+    await assertFetch(root, call, "GET", expected);
+  });
 
-    // Action
-    const client = new SendenClient();
-    const cdn = client.build<any>("/cdn/api");
-    client.fetch = mock.fetch;
-    await cdn.example.$get({ $test: "", $abc: 123, abcd: "12345" });
+  it("example.com/", async () => {
+    // Setup
+    global.location = { origin: "http://localhost" } as any;
+    const root = "https://example.com";
+    const params = {};
+    const call = (r: any) => r.$get(params);
+    // Assert
+    const expected = "https://example.com/";
+    await assertFetch(root, call, "GET", expected);
+  });
 
-    // Assertions
-    expect(mock.fetch).toBeCalledTimes(1);
-    const request: Request = mock.args[0];
-    expect(request.url).toEqual("http://localhost/cdn/api/example?test=&abc=123");
-    expect(request.method).toEqual("GET");
-    expect(request.body).toEqual(null);
-    expect(Array.from(request.headers.entries())).toEqual([["content-type", "application/json"]]);
+  it("example/123?test=", async () => {
+    // Setup
+    global.location = { origin: "http://localhost" } as any;
+    const root = "/cdn/api";
+    const params = { $test: "", $abc: 123 };
+    const call = (r: any) => r.example.$abc.$get(params);
+    // Assert
+    const expected = "http://localhost/cdn/api/example/123?test=";
+    await assertFetch(root, call, "GET", expected);
+  });
+
+  it("example?test=%2522123%2522&abc=123", async () => {
+    // Setup
+    global.location = { origin: "http://localhost" } as any;
+    const root = "/cdn/api";
+    const params = { $test: "123", $abc: 123 };
+    const call = (r: any) => r.example.$get(params);
+    // Assert
+    const expected = "http://localhost/cdn/api/example?test=%2522123%2522&abc=123";
+    await assertFetch(root, call, "GET", expected);
+  });
+
+  it("DELETE example?test=&abc=123", async () => {
+    // Setup
+    global.location = { origin: "http://localhost" } as any;
+    const root = "/cdn/api";
+    const params = { $test: "", $abc: 123 };
+    const call = (r: any) => r.example.$delete(params);
+    // Assert
+    const expected = "http://localhost/cdn/api/example?test=&abc=123";
+    await assertFetch(root, call, "DELETE", expected);
+  });
+
+  it("POST example?test=&abc=123", async () => {
+    // Setup
+    global.location = { origin: "http://localhost" } as any;
+    const root = "/cdn/api";
+    const params = { $test: "", $abc: 123, test: "-123", abc: -123 };
+    const call = (r: any) => r.example.$post(params);
+    // Assert
+    const expected = "http://localhost/cdn/api/example?test=&abc=123";
+    const body = { test: "-123", abc: -123 };
+    await assertFetch(root, call, "POST", expected, body);
   });
 });
